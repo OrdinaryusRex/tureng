85ca37018fc0796f2c2ef8edd67a65aa7660489a
Emin Ali Ipekdokuyan <65105310+OrdinaryusRex@users.noreply.github.com>
2020-09-10 07:51:15 +0300

    Update tureng.py

--- tureng.py
+++ tureng.py
@@ -3,14 +3,18 @@ import sys
 import pandas as pd
 import tabulate
 from ftfy import fix_encoding
-u = "https://tureng.com/tr/turkce-ingilizce/"
+u = "https://tureng.com/en/turkish-english/"
 rl = sys.argv
 rl = rl[1]
 url = u + rl
 # page = pd.read_html(url, index_col=0, encoding='utf-8')
 page = pd.read_html(url, encoding='utf-8')
-
-table = page[0]
+print(len(page))
+# print(page[1]['Category'])
+# print(page[1]['English'])
 # table = table.drop(table.columns[3], axis=1)
 
-print(table)
+df = page[0]
+df = df[['Category','Turkish','English']]
+df = df.dropna(subset = ['Turkish'])
+print(df)
