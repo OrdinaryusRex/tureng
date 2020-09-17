ba9c5d7925e4454e4b7eba75edb0102700ee73d5
Emin Ali Ipekdokuyan <65105310+OrdinaryusRex@users.noreply.github.com>
2020-09-17 11:52:05 +0300

    Update tureng.py

--- tureng.py
+++ tureng.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 import sys
 import pandas as pd
-import tabulate
+from tabulate import tabulate
 from ftfy import fix_encoding
 u = "https://tureng.com/en/turkish-english/"
 rl = sys.argv
@@ -9,12 +9,21 @@ rl = rl[1]
 url = u + rl
 # page = pd.read_html(url, index_col=0, encoding='utf-8')
 page = pd.read_html(url, encoding='utf-8')
-print(len(page))
 # print(page[1]['Category'])
 # print(page[1]['English'])
 # table = table.drop(table.columns[3], axis=1)
 
-df = page[0]
-df = df[['Category','Turkish','English']]
-df = df.dropna(subset = ['Turkish'])
-print(df)
+t1 = page[0]
+t1 = t1[['Category','Turkish','English']]
+t1 = t1.dropna(subset = ['Turkish'])
+
+# t2 = page[1]
+# t2 = t2[['Turkish','English']]
+# t2 = t2.dropna(subset = ['Turkish'])
+
+t1 = t1.values.tolist()
+# t2 = t2.values.tolist()
+
+headers = ['Category', 'Turkish', 'English']
+print(tabulate(t1[:3], headers, tablefmt="grid"))
+# print(tabulate(t2[:3], tablefmt="grid"))
