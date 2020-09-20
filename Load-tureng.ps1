647434307b9d0c0d175d810eede40356c765a88e
Emin Ali Ipekdokuyan <65105310+OrdinaryusRex@users.noreply.github.com>
2020-09-20 20:32:11 +0300

    Create Load-tureng.ps1

---
+++ Load-tureng.ps1
@@ -0,0 +1,12 @@
+﻿function tureng {
+    $param=$args[0]
+    $param2=$args[1]
+    $loc = Get-Location
+    Set-Location C:\Users\EMINA\desktop\tureng
+    .\env\Scripts\Activate.ps1
+    if (!$param2) { $kelime=$param }
+    if ($param2) { $kelime=$param + '%20' + $param2 }
+    python .\tureng.py $kelime
+    Set-Location $loc
+    deactivate
+}
